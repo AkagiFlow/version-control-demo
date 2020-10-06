b33b24c387abe1099f7002d41f3e4e9fa75c23df
AkagiFlow <72470996+AkagiFlow@users.noreply.github.com>
2020-10-06 14:42:26 -0700

    Updated code with full functionality + main method for testing.

--- CSE 360 Assignment 1/src/cse360assignment02/AddingMachine.java
+++ CSE 360 Assignment 1/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,40 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String str = "0 ";
   
-  public AddingMachine () {
+  public static void main(String[] args) { //main method for testing
+	 AddingMachine myCalculator = new AddingMachine();
+	 myCalculator.add(4);
+	 myCalculator.subtract(2);
+	 myCalculator.add(5);
+	 String str = myCalculator.toString();
+	 System.out.println(str);
+  }
+  
+  public AddingMachine () { //class object constructor
     total = 0;  // not needed - included for clarity
   }
   
-  public int getTotal () {
-    return 0;
+  public int getTotal () { //returns current total
+    return total;
   }
   
-  public void add (int value) {
+  public void add (int value) { //adds parameter to current total
+	  total += value;
+	  str += "+ " + value + " ";
   }
 
-  public void subtract (int value) {
+  public void subtract (int value) {//subtracts parameter from current total
+	  total -= value;
+	  str += "- " + value + " ";
   }
 
-  public String toString () {
-    return "";
+  public String toString () { //returns a string of operations
+    return str;
   }
 
-  public void clear() {
+  public void clear() { //resets total to 0
+	  total = 0;
   }
 }
